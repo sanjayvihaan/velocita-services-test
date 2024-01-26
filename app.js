1d11cf3ae1f17a398fecc0b5717a5763afb8e5ce
V Sanjay <vsanjay@Vs-MacBook-Air.local>
2024-01-26 22:20:51 +0530

    first commit

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const {
 const { handleGetListOfCoords } = require('./controllers/externalApi')
 const { realTimeLocationSystem } = require('./controllers/rtls')
 
+// Initialize Redis client
+// const redisClient = redis.createClient();
+
+// // Handle connection errors
+// redisClient.on('error', (err) => {
+//   console.error(`Redis client error: ${err}`);
+// });
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
@@ -114,18 +122,44 @@ io.on('connection', (socket) => {
     }
   })
 
-  socket.on('amb-loc-upd', async ({ coordinates }) => {
-    const res = await realTimeLocationSystem(socket.id, coordinates)
-    if (res == 'success') {
-      console.log(`Location update successfull - ${socket.id}`)
-      io.to(socket.id).emit('amb-loc-upd-status', {})
-    } else if (res == 'error') {
-      console.log(`Error at location update - ${socket.id}`)
-      io.to(socket.id).emit('amb-loc-upd-status', {
-        msg: 'Error updating location',
-      })
-    }
-  })
+  // Function to get nearby ambulances using GEOSEARCH
+const getNearbyAmbulances = async (pincode, longitude, latitude, radius) => {
+  try {
+    // Use GEOSEARCH to get all nearby ambulances within the specified radius
+    const nearbyAmbulances = await redisClient.geosearch(
+      pincode,
+      longitude,
+      latitude,
+      'BYRADIUS',
+      radius,
+      'm'
+    );
+
+    // Return the list of nearby ambulance IDs
+    return nearbyAmbulances;
+  } catch (error) {
+    console.error('Error fetching nearby ambulances:', error);
+    return [];
+  }
+};
+
+socket.on('get-all-nearby-ambulances', async function ({ coordinates }) {
+  // Fetch all nearby ambulance IDs
+  const allNearbyAmbulances = await getNearbyAmbulances(
+    coordinates.pin,
+    coordinates.lon,
+    coordinates.lat,
+    5000 // Adjust the radius as needed
+  );
+
+  // Emit the list of nearby ambulances to the requesting client
+  io.to(socket.id).emit('all-nearby-ambulances', allNearbyAmbulances);
+  console.log("all ambulance", allNearbyAmbulances);
+});
+
+
+
+  
 })
 
 server.listen(PORT, () => {
